d9f3c510a5186475871de14a9e2e71a91fa82f09
mANu <correomanurivas@gmail.com>
2025-02-15 17:47:32 +0100

    a

--- script.js
+++ script.js
@@ -6,27 +6,22 @@ let arrayComida = [
     {x: 0, y: 0}
 ];
 
-let superviviente = document.querySelector(".superviviente");
+let anchoSuperviviente = 50;
+let altoSuperviviente = 50;
 let coordenadasAleatorias = [];
-let listaSupervivientes = document.getElementsByClassName("superviviente");
-posicionarSupervivientes(listaSupervivientes);
-
-
 let anchoComida = 50;
 let altComida = 50;
 
-//Genera posición aleatoria para las comida sin que se sobrelapan
-function generaPosicionComida(contenedorComidaX, contenedorComidaY){
-    for (let i=0; i<arrayComida.length; i++){
-        let randomX;
-        let randomY;
-        let overlap;
-        do{
+// Genera posición aleatoria para las comida sin que se sobrelapan
+function generaPosicionComida(contenedorComidaX, contenedorComidaY) {
+    for (let i = 0; i < arrayComida.length; i++) {
+        let randomX, randomY, overlap;
+        do {
             randomX = Math.random() * (contenedorComidaX - anchoComida);
             randomY = Math.random() * (contenedorComidaY - altComida);
             overlap = false;
-            for (let j=0; j<i; j++) {
-                if (Math.abs(randomX - arrayComida[j].x) < anchoComida && Math.abs(randomY - arrayComida[j].y) < altComida){
+            for (let j = 0; j < i; j++) {
+                if (Math.abs(randomX - arrayComida[j].x) < anchoComida && Math.abs(randomY - arrayComida[j].y) < altComida) {
                     overlap = true;
                     break;
                 }
@@ -37,9 +32,10 @@ function generaPosicionComida(contenedorComidaX, contenedorComidaY){
     }
     pintarComidas();
 }
+
 // Pinta las comidas
-function pintarComidas(contenedorComida){
-    for (let i=0; i<arrayComida.length; i++){
+function pintarComidas(contenedorComida) {
+    for (let i = 0; i < arrayComida.length; i++) {
         let divComida = document.createElement('div');
         divComida.classList.add('comida');
         divComida.style.top = arrayComida[i].y + 'px';
@@ -50,8 +46,8 @@ function pintarComidas(contenedorComida){
 
 // Aparición aleatoria de supervivientes.
 function aleatorio() {
-    let coordRandomX = Math.round(Math.random()* window.innerWidth);
-    let coordRandomY = Math.round(Math.random()* window.innerHeight);
+    let coordRandomX = Math.round(Math.random() * window.innerWidth);
+    let coordRandomY = Math.round(Math.random() * window.innerHeight);
 }
 
 // Musica
@@ -60,31 +56,47 @@ function r1() {
     audio.src = 'media/creedence.mp3';
     audio.play();
 }
+
 function r2() {
     audio.pause();
     audio.src = 'media/wagner.mp3';
     audio.play();
 }
+
 function r3() {
     audio.pause();
     audio.src = 'media/acdc.mp3';
     audio.play();
 }
-function mute(){
+
+function mute() {
     audio.pause();
 }
+
+// Aparición aleatoria de supervivientes.
 function coordenadasAleatoriasSupervivientes() {
-    let coordRandomX = Math.round(Math.random()* window.innerWidth - superviviente.offsetWidth);
-    let coordRandomY = Math.round(Math.random()* window.innerHeight - superviviente.offsetHeight);
-    coordenadasAleatorias = [coordRandomX, coordRandomY];
+    let coordRandomX = Math.round(Math.random() * (window.innerWidth - anchoSuperviviente));
+    let coordRandomY = Math.round(Math.random() * (window.innerHeight - altoSuperviviente));
+    return [coordRandomX, coordRandomY];
 }
 
+// Posiciona a los supervivientes en la pantalla
 function posicionarSupervivientes(supervivientes) {
     for (let i = 0; i < supervivientes.length; i++) {
-        coordenadasAleatoriasSupervivientes();
-        let coordenadas = coordenadasAleatorias; 
+        let coordenadas = coordenadasAleatoriasSupervivientes();
         supervivientes[i].style.left = coordenadas[0] + 'px';
         supervivientes[i].style.top = coordenadas[1] + 'px';
     }
 }
 
+// Obtiene la lista de supervivientes y los posiciona cuando la página carga
+window.onload = function() {
+    let elementos = document.getElementsByClassName("superviviente");
+    let listaSupervivientes = [];
+
+    for (let i = 0; i < elementos.length; i++) {
+        listaSupervivientes[i] = elementos[i];
+    }
+
+    posicionarSupervivientes(listaSupervivientes);
+};
