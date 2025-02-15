2e0f7155510a6834aceab601b529ef3fd542dd5e
mANu <correomanurivas@gmail.com>
2025-02-15 14:22:09 +0100

    a

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ let arrayComida = [
     {x: 0, y: 0}
 ];
 
+let superviviente = document.querySelector(".superviviente");
+let coordenadasAleatorias = [];
+let listaSupervivientes = document.getElementsByClassName("superviviente");
+posicionarSupervivientes(listaSupervivientes);
+
+
 let anchoComida = 50;
 let altComida = 50;
 
@@ -45,21 +51,21 @@ function pintarComidas(contenedorComida){
 // Aparición aleatoria de supervivientes.
 
 function coordenadasAleatoriasSupervivientes() {
-    let coordenadasAleatorias = [];
-    let coordRandomX = Math.round(Math.random()* window.innerWidth);
-    let coordRandomY = Math.round(Math.random()* window.innerHeight);
-
+    let coordRandomX = Math.round(Math.random()* window.innerWidth - superviviente.offsetWidth);
+    let coordRandomY = Math.round(Math.random()* window.innerHeight - superviviente.offsetHeight);
     coordenadasAleatorias = [coordRandomX, coordRandomY];
-
-    return coordenadasAleatorias;
 }
 
 function posicionarSupervivientes(supervivientes) {
     for (let i = 0; i < supervivientes.length; i++) {
-        let coordenadas = coordenadasAleatoriasSupervivientes(); 
+        coordenadasAleatoriasSupervivientes();
+        let coordenadas = coordenadasAleatorias; 
         supervivientes[i].style.left = coordenadas[0] + 'px';
         supervivientes[i].style.top = coordenadas[1] + 'px';
     }
 }
+
+
+
 
 
